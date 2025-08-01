31c3aa1d7024824feff8ba9cebae9c7cadb71094
吴健0125 <2457507566@qq.com>
2025-08-01 22:48:17 +0800

    feat(user): 添加用户相关功能- 新增用户注册、登录、登出功能 - 实现用户信息查询和更新接口 - 添加用户权限控制，区分普通用户和管理员 - 生成用户相关实体类和数据访问层

--- src/main/java/com/wj/vegetablebackend/VegetableBackendApplication.java
+++ src/main/java/com/wj/vegetablebackend/VegetableBackendApplication.java
@@ -1,9 +1,11 @@
 package com.wj.vegetablebackend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.wj.vegetablebackend.mapper")
 public class VegetableBackendApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/wj/vegetablebackend/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.wj.vegetablebackend.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.wj.vegetablebackend.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author wj
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
